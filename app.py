5a5d0c21dd6927905db6e5c4174464321bf6e9c2
Amandaxs <39634209+Amandaxs@users.noreply.github.com>
2023-04-03 17:13:11 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from selenium import webdriver
+import pandas as pd
+import time
+import json
+from datetime import datetime
+import pathlib
+import glob
+import sys
+sys.path.append(".")
+import faker
+import numpy as np
+import pandas as pd
+from selenium.webdriver.common.by import By
+import time
+import datetime as dt
+
+
+st.write("""
+    # Classificação de ruídos
+
+    **Este App é destinado a demonstração do modelo de classificação de ruido**""")
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+f = faker.Faker()
+colors = ["Sim","Não"]
+nome = "Julia"
+
+url = "https://docs.google.com/forms/d/e/1FAIpQLSdo7cVObaa9W5K0Dpe_ndeO6BDlvojeYsIKC0JKQ2MczPGJCg/viewform?usp=sf_link"
+driver = webdriver.Chrome(executable_path="./chromedriver",chrome_options=options )
+
+driver.get(url)
+time.sleep(2)
+
+textboxes = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
+textboxes.send_keys("App")
+data = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+data.send_keys(str(dt.datetime.now()))
+
+resp = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+resp.send_keys("Sim")
+
+submit = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+submit.click()
+
+st.write("""
+    # Enviado com sucesso""")
