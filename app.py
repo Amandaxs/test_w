d56ffee460cfcfb90d8b54df476388997de73480
Amandaxs <39634209+Amandaxs@users.noreply.github.com>
2023-04-11 09:52:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from selenium import webdriver
 import pandas as pd
 import time
 import json
@@ -10,60 +11,49 @@ sys.path.append(".")
 import faker
 import numpy as np
 import pandas as pd
+from selenium.webdriver.common.by import By
 import time
 import datetime as dt
 
 
+option = st.radio('Você concorda com a classificão dada pelo modelo:',
+                  ['Sim',
+                   'Não'])
 
+st.write('You selected:', option)
 
 st.write("""
     # Classificação de ruídos
 
     **Este App é destinado a demonstração do modelo de classificação de ruido**""")
 
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
 
 
+if st.button('enviar resposta'):
+    f = faker.Faker()
+    colors = ["Sim","Não"]
+    nome = "Julia"
 
-import streamlit as st
-import os, sys
+    url = "https://docs.google.com/forms/d/e/1FAIpQLSdo7cVObaa9W5K0Dpe_ndeO6BDlvojeYsIKC0JKQ2MczPGJCg/viewform?usp=sf_link"
+    driver = webdriver.Chrome(executable_path="./chromedriver",chrome_options=options )
 
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.10/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+    driver.get(url)
+    time.sleep(2)
 
-_ = installff()
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-driver = webdriver.Firefox('/home/appuser/venv/lib/python3.10/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver.exe',options=opts)
+    textboxes = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
+    textboxes.send_keys("AppLocal")
+    data = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+    data.send_keys(str(dt.datetime.now()))
 
+    resp = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input' )
+    resp.send_keys(option)
 
+    submit = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+    submit.click()
 
-f = faker.Faker()
-colors = ["Sim","Não"]
-nome = "Julia"
-
-url = "https://docs.google.com/forms/d/e/1FAIpQLSdo7cVObaa9W5K0Dpe_ndeO6BDlvojeYsIKC0JKQ2MczPGJCg/viewform?usp=sf_link"
-
-#s=Service(ChromeDriverManager().install()) ##
-#driver = webdriver.Chrome(executable_path=s,chrome_options=options )
-
-
-driver.get(url)
-time.sleep(2)
-
-textboxes = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div/div[1]/input')
-textboxes.send_keys("App")
-data = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input' )
-data.send_keys(str(dt.datetime.now()))
-
-resp = driver.find_element('xpath','//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input' )
-resp.send_keys("Sim")
-
-submit = driver.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
-submit.click()
-
-st.write("""
-    # Enviado com sucesso""")
+    st.write("""
+        # Enviado com sucesso""")
